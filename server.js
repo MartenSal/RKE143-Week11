55d7323a463150ab67f826adc72b7b49b11db7a5
Märten Salong <marten.salong@tktk.ee>
2025-09-22 18:58:33 +0300

    Week 12

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const express = require('express');
-const app = express();
 const recipeRouter = require('./routes/recipes.routes');
 const ingredientRouter = require('./routes/ingredients.routes');
 const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const randomRouter = require('./routes/randomRecipe.routes');
-const db = require('./db');
+const app = express();
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(express.json());
 
-app.use('/ingredients', ingredientRouter)
+app.use('/ingredients', ingredientRouter);
 app.use('/recipes', recipeRouter);
 app.use('/fullRecipes', fullRecipesRouter);
 app.use('/random', randomRouter);
 
-
-app.listen(3000, () =>  {
+app.listen(3000, () => {
     console.log('Server is running on Port 3000.');
 });
